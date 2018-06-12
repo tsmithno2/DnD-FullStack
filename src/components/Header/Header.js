9771231fd7dc82932c41416d5eefce4b6e449d1a
tsmithno2 <t.smithno2@gmail.com>
2018-06-12 15:49:22 -0600

    buttons in the header work with links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
       <h1>Header</h1>
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
+
+      <Link to="/playing">
+        <button>Playing</button>
+      </Link>
+
+      <Link to="/newcampaign">
+        <button>New Campaign</button>
+      </Link>
+
+      <Link to="/newcharacter">
+        <button>New Character</button>
+      </Link>
+
+      <Link to="">
+        <button>Log Out</button>
+      </Link>
+
       <hr />
     </div>
   );
