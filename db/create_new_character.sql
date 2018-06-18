b5c011db72f2bc3b7b688d62562531dd693d5b2e
tsmithno2 <t.smithno2@gmail.com>
2018-06-18 12:38:41 -0600

    update new charater creation and created dummy data on table resets

--- db/create_new_character.sql
+++ db/create_new_character.sql
@@ -1,5 +1,5 @@
 insert into chracters 
-(party_id, cpq_id, char_npc, char_pc, char_name, char_picture, char_alignment, char_deity, char_strength, char_dexterity, char_constitution, char_intelligence, char_wisdom, char_charisma, char_inventory, char_dm_notes)
+(party_id, camp_id, char_npc, char_pc, char_name, char_picture, char_alignment, char_deity, char_strength, char_dexterity, char_constitution, char_intelligence, char_wisdom, char_charisma, char_inventory, char_dm_notes)
 values
 ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
 returning *;
