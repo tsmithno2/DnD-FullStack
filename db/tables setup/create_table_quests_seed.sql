f633dae9b428c442cd05bd2d1c8ef5c0fec261b4
tsmithno2 <t.smithno2@gmail.com>
2018-06-25 09:34:41 -0600

    table updated for quests

--- db/tables setup/create_table_quests_seed.sql
+++ db/tables setup/create_table_quests_seed.sql
@@ -1,5 +1,7 @@
 create table quests (
     quest_id serial primary key,
+    camp_id integer
+    references campaigns(camp_id),
     quest_name varchar (80),
     quest_description text,
     quest_picture text,
