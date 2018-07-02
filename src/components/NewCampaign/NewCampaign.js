bf58f1260b562f7e12789ca73b79d90f7fbbf2a1
tsmithno2 <t.smithno2@gmail.com>
2018-07-01 20:40:37 -0600

    All views except auth0 and login done

--- src/components/NewCampaign/NewCampaign.js
+++ src/components/NewCampaign/NewCampaign.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createCampaign } from "../../dux/reducer";
+import "./NewCampaign.css";
 
 class NewCampaign extends Component {
   constructor() {
@@ -34,44 +35,46 @@ class NewCampaign extends Component {
 
   render() {
     return (
-      <div>
-        <h1>New Campaign</h1>
-        <p>
-          Campaign Picture (URL's ONLY){" "}
-          <input
-            value={this.state.camp_picture}
-            onChange={e => this.setState({ camp_picture: e.target.value })}
-          />
-        </p>
+      <div className="WholePageDisplay">
+        <div className="NewCampaignCard">
+          <h1>New Campaign</h1>
+          <p>
+            Campaign Picture (URL's ONLY){" "}
+            <input
+              value={this.state.camp_picture}
+              onChange={e => this.setState({ camp_picture: e.target.value })}
+            />
+          </p>
 
-        <p>
-          Campaign Name{" "}
-          <input
-            value={this.state.camp_name}
-            onChange={e => this.setState({ camp_name: e.target.value })}
-          />
-        </p>
+          <p>
+            Campaign Name{" "}
+            <input
+              value={this.state.camp_name}
+              onChange={e => this.setState({ camp_name: e.target.value })}
+            />
+          </p>
 
-        <p>
-          One Senence Campaign Description{" "}
-          <input
-            value={this.state.camp_desc1}
-            onChange={e => this.setState({ camp_desc1: e.target.value })}
-          />
-        </p>
+          <p>
+            One Senence Campaign Description{" "}
+            <input
+              value={this.state.camp_desc1}
+              onChange={e => this.setState({ camp_desc1: e.target.value })}
+            />
+          </p>
 
-        <p>
-          Full Campaign Description{" "}
-          <input
-            value={this.state.camp_desc2}
-            onChange={e => this.setState({ camp_desc2: e.target.value })}
-          />
-        </p>
-        <Link to="/home">
-          <button onClick={() => this.clickSave()}>Create</button>
-        </Link>
+          <p>
+            Full Campaign Description{" "}
+            <input
+              value={this.state.camp_desc2}
+              onChange={e => this.setState({ camp_desc2: e.target.value })}
+            />
+          </p>
+          <Link to="/home">
+            <button onClick={() => this.clickSave()}>Create</button>
+          </Link>
 
-        <button onClick={() => this.clickCancel()}>Reset All Fields</button>
+          <button onClick={() => this.clickCancel()}>Reset All Fields</button>
+        </div>
       </div>
     );
   }
--- src/components/Quests/NewQuest/NewQuest.js
+++ src/components/Quests/NewQuest/NewQuest.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./NewQuest.css";
 
 export default class NewQuest extends Component {
   constructor() {
@@ -57,44 +58,48 @@ export default class NewQuest extends Component {
   render() {
     console.log("render state ", +this.props.match.params.campaignid);
     return (
-      <div>
-        <h1>Create New Quest</h1>
-        <p>
-          Quest Name{" "}
-          <input
-            value={this.state.quest_name}
-            onChange={e => this.setState({ quest_name: e.target.value })}
-          />
-        </p>
-        <p>
-          Quest Picture{" "}
-          <input
-            value={this.state.quest_picture}
-            onChange={e => this.setState({ quest_picture: e.target.value })}
-          />
-        </p>
-        <p>
-          Quest Description{" "}
-          <input
-            value={this.state.quest_description}
-            onChange={e => this.setState({ quest_description: e.target.value })}
-          />
-        </p>
-        <select
-          onChange={e => {
-            this.dropdown(e);
-          }}
-        >
-          <option value="Unobtained">Unobtained</option>
-          <option value="Obtained">Obtained</option>
-          <option value="Completed">Completed</option>
-        </select>
-        <br />
-        <Link to={`/playing/${this.state.camp_id}`}>
-          <button onClick={() => this.clickSave()}>Save</button>
-        </Link>
-        <button onClick={() => this.clickCancel()}>Cancel</button>
-        <div />
+      <div className="FullNewQuest">
+        <div className="NewQuestCard">
+          <h1>Create New Quest</h1>
+          <p>
+            Quest Name
+            <input
+              value={this.state.quest_name}
+              onChange={e => this.setState({ quest_name: e.target.value })}
+            />
+          </p>
+          <p>
+            Quest Picture
+            <input
+              value={this.state.quest_picture}
+              onChange={e => this.setState({ quest_picture: e.target.value })}
+            />
+          </p>
+          <p>
+            Quest Description
+            <input
+              value={this.state.quest_description}
+              onChange={e =>
+                this.setState({ quest_description: e.target.value })
+              }
+            />
+          </p>
+          <select
+            onChange={e => {
+              this.dropdown(e);
+            }}
+          >
+            <option value="Unobtained">Unobtained</option>
+            <option value="Obtained">Obtained</option>
+            <option value="Completed">Completed</option>
+          </select>
+          <div>
+            <Link to={`/playing/${this.state.camp_id}`}>
+              <button onClick={() => this.clickSave()}>Save</button>
+            </Link>
+            <button onClick={() => this.clickCancel()}>Cancel</button>
+          </div>
+        </div>
       </div>
     );
   }
